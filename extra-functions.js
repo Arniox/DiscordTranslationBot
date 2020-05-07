645818527c337a102527b3e5482770bf5ff396aa
Arniox <nikkdiehl@gmail.com>
2020-05-07 16:51:36 +1200

    First Commit

---
+++ extra-functions.js
@@ -0,0 +1,190 @@
+module.exports = {
+    //Random
+    siteRand: function (high, low) {
+        return Math.random() * (high - low) + low;
+    },
+    //Maps to Math.ceil
+    ceil: function (num) {
+        return Math.ceil(num);
+    },
+    //Maps to Math.floor
+    floor: function (num) {
+        return Math.floor(num);
+    },
+}
+
+//Number Ordinal
+Number.prototype.ordinal = function () {
+    var ones = this % 10;
+    var tens = (this / 10).floor() % 10;
+    var stuff = "";
+
+    if (tens == 1) {
+        stuff = "th";
+    } else {
+        switch (ones) {
+            case 1:
+                stuff = "st";
+                break;
+            case 2:
+                stuff = "nd";
+                break;
+            case 3:
+                stuff = "rd";
+                break;
+            default:
+                stuff = "th";
+        }
+    }
+
+    return this.toString() + stuff;
+}
+
+//Pad a number and return as string
+Number.prototype.pad = function (size) {
+    return ('0000000000000000000000000000000000000000' + this).substr(-size);
+}
+
+//Cut / Truncate a number to fixed value
+Number.prototype.toFixedCut = function (digits) {
+    var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
+        m = this.toString().match(re);
+    return m ? parseFloat(m[1]) : this.valueOf();
+};
+
+//Format the number into a properly written number
+Number.prototype.formatComma = function () {
+    return this.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+//Is this number an int
+Number.prototype.isInt = function () {
+    return this % 1 == 0;
+}
+
+//Format the string seconds to the full HHMMSS time format
+String.prototype.toHHMMSS = function (en = false) {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) { hours = (!en ? "0" : "") + hours; }
+    if (minutes < 10) { minutes = (!en ? "0" : "") + minutes; }
+    if (seconds < 10) { seconds = (!en ? "0" : "") + seconds; }
+
+    if (!en) {
+        return hours + ':' + minutes + ':' + seconds;
+    } else {
+        return hours + ' hours, ' + minutes + ' minutes, and ' + seconds + ' seconds';
+    }
+}
+
+//Find last element in array
+Array.prototype.last = function () {
+    return this[this.length - 1];
+}
+
+//Find first element in array
+Array.prototype.first = function () {
+    return this[0];
+}
+
+//Find the max item in an array
+Array.prototype.max = function () {
+
+    var obj;
+    var maxNum = -Infinity;
+    var index = -Infinity;
+
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] >= maxNum) {
+            maxNum = this[i];
+            index = i;
+        }
+    }
+
+    obj = {
+        maxNum: maxNum,
+        index: index,
+    }
+
+    return obj;
+}
+
+//Remove specific element in array
+Array.prototype.removeElement = function (elementToRemove) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] === elementToRemove) {
+            this.splice(i, 1);
+        }
+    }
+
+    return this;
+}
+
+//Maps to Math.abs
+Number.prototype.abs = function () {
+    return Math.abs(this);
+}
+//Maps to Math.ceil
+Number.prototype.ceil = function () {
+    return Math.ceil(this);
+}
+//Maps to Math.floor
+Number.prototype.floor = function () {
+    return Math.floor(this);
+}
+//Maps to Math.max
+Number.prototype.max = function (low) {
+    return Math.max(this, low);
+}
+//Maps to Math.min
+Number.prototype.min = function (high) {
+    return Math.min(this, high);
+}
+//Constrains a number between two values
+Number.prototype.constrain = function (low, high) {
+    return this.max(this.min(high), low);
+}
+//Maps to Math.pow
+Number.prototype.pow = function (n) {
+    return Math.pow(this, n);
+}
+//Maps to Math.exp
+Number.prototype.exp = function () {
+    return Math.E.pow(this);
+}
+//Maps to Math.log
+Number.prototype.log = function () {
+    return Math.log(this);
+}
+//Maps a given number from another range to a different range
+Number.prototype.map = function (start1, stop1, start2, stop2, withinBounds = false) {
+    var newVal = (this - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+    if (!withinBounds) {
+        return newVal;
+    }
+    if (start2 < stop2) {
+        return this.constrain(newVal, start2, stop2);
+    } else {
+        return this.constrain(newVal, stop2, start2);
+    }
+}
+//Normalizes a number from another range into a a value btween 0 and 1
+Number.prototype.norm = function (start, stop) {
+    return this.map(start, stop, 0, 1);
+}
+//Maps to Math.round
+Number.prototype.round = function () {
+    return Math.round(this);
+}
+//Maps to Math.sq
+Number.prototype.sq = function () {
+    return this * this;
+}
+//Maps to Math.sqrt
+Number.prototype.sqrt = function () {
+    return Math.sqrt(this);
+}
+
