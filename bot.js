3d0e621bb855928fb3344e1955ee53fa64b2b84e
Arniox <nikkdiehl@gmail.com>
2020-05-08 02:32:43 +1200

    Added country data

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ var Discord = require('discord.js');
 var settings = require('./configure.json');
 var dataToUse = require('./data-to-use.json');
 var tools = require('./extra-functions');
+var countryData = require('country-data.json');
 var fs = require('fs');
 var googleTranslate = require('google-translate')(googleApiKey, { "concurrentLimit": 20 });
 
@@ -312,7 +313,8 @@ bot.on('message', msg => {
 				//Translate if not english or link
 				if (detection.language != 'en' && detection.language != 'und') {
 					googleTranslate.translate(msgContent, detection.language, 'en', function (err, translation) {
-						return channel.send(translation.translatedText);
+						var country = countryData.find(i => i.ISOCode == detection.language);
+						return channel.send(translation.translatedText + ' - original translated from ' + country.name);
 					});
 				}
 			});
