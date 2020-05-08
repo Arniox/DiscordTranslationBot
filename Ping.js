7317015c21a1659fea5f2ee2a0defa80169ce1f0
Arniox <nikkdiehl@gmail.com>
2020-05-08 20:08:48 +1200

    Fix for ping command

--- Ping.js
+++ Ping.js
@@ -1,6 +1,6 @@
 module.exports = {
     //Ping command
-    ping: function (msgContent, channel, author, member, guild) {
+    ping: function (msg, msgContent, channel, author, member, guild) {
         msg.delete({ timeout: 0 }); //Delete message
         return channel.send(new Discord.MessageEmbed().setDescription('Right back at you! Yes, I am alive. Current uptime is: ' +
             UpTime() + '. Current Prefix is: ' + settings.prefix));
