b956a3df3a09c9c2642782b1fe4453888d5c32e5
Arniox <nikkdiehl@gmail.com>
2020-05-08 20:01:11 +1200

    Testing

---
+++ Ping.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //Ping command
+    ping: function (msgContent, channel, author, member, guild) {
+        msg.delete({ timeout: 0 }); //Delete message
+        return channel.send(new Discord.MessageEmbed().setDescription('Right back at you! Yes, I am alive. Current uptime is: ' +
+            UpTime() + '. Current Prefix is: ' + settings.prefix));
+    }
+}
--- bot.js
+++ bot.js
@@ -11,6 +11,9 @@ var tools = require('./extra-functions');
 var fs = require('fs');
 var googleTranslate = require('google-translate')(googleApiKey, { "concurrentLimit": 20 });
 
+//Functions
+const Ping = require('./Ping.js');
+
 //Initialize Discord bot 
 const bot = new Discord.Client();
 
@@ -40,9 +43,7 @@ bot.on('message', msg => {
 
 		switch (cmd) {
 			case 'ping': //Ping the bot - Are you alive
-				msg.delete({ timeout: 0 }); //Delete message
-				return channel.send(new Discord.MessageEmbed().setDescription('Right back at you! Yes, I am alive. Current uptime is: ' +
-					UpTime() + '. Current Prefix is: ' + settings.prefix));
+				return Ping(msgContent, channel, author, member, guild);
 			case 'prefix': //Prefix options
 				msg.delete({ timeout: 0 }); //Delete message
 				if (args.length != 0) {
