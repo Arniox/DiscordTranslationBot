0bd02b835b3b56c920731d2102463ef9b9b643b0
Arniox <nikkdiehl@gmail.com>
2020-05-10 02:20:08 +1200

    Moved message embedd creation and sending back into the function
    -  but send doesn't return
    - Return is now outside.

--- bot.js
+++ bot.js
@@ -210,11 +210,9 @@ bot.on('message', msg => {
 							//Send embedded message
 							return channel.send(embeddedHelpMessage);
 						case 'nick':
-							//Emtpy embedd
-							var embeddedHelpMessage;
 							//Get all available language codes
 							googleTranslate.getSupportedLanguages(function (err, languageCodes) {
-								embeddedHelpMessage = new Discord.MessageEmbed()
+								var embeddedHelpMessage = new Discord.MessageEmbed()
 									.setColor('#0099ff')
 									.setAuthor(bot.user.username, bot.user.avatarURL())
 									.setDescription('Nick allows you to translate either you\'re own nickname into any supported language, or everyone\'s granted you have management permissions.')
@@ -237,11 +235,13 @@ bot.on('message', msg => {
 									)
 									.setTimestamp()
 									.setFooter('Thanks, and have a good day');
+
+								//Send embedded message
+								channel
+									.send(embeddedHelpMessage)
+									.catch(error => { console.log('Error. Ignored') });
 							});
-							//Send embedded message
-							return channel
-								.send(embeddedHelpMessage)
-								.catch(error => { console.log('Error. Ignored') });
+							return;
 						case 'prykie':
 							var embeddedHelpMessage = new Discord.MessageEmbed()
 								.setColor('#0099ff')
