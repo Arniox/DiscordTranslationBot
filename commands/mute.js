220f7a7cc615e50bfbc672712dd5c93ebd4fa01f
Arniox <nikkdiehl@gmail.com>
2020-05-20 21:22:04 +1200

    Changed help command for mute to error color

--- commands/mute.js
+++ commands/mute.js
@@ -47,7 +47,7 @@ function HelpMessage(bot, message, args) {
     var randomChannel = message.guild.channels.cache.filter(i => i.type == 'voice').random().name;
 
     var embeddedHelpMessage = new Discord.MessageEmbed()
-        .setColor('#0099ff')
+        .setColor('#b50909')
         .setAuthor(bot.user.username, bot.user.avatarURL())
         .setDescription('The mute command allows you to server mute everyone in a selected voice channel barring mute ignored roles. This works fine with spaces in the name and is case insensitive.')
         .addFields(
