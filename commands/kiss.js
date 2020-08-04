c360f105580a203a341e10cfdd9cd0cfa640788e
Arniox <nikkdiehl@gmail.com>
2020-08-04 17:02:04 +1200

    Send learderboard message

--- commands/kiss.js
+++ commands/kiss.js
@@ -13,20 +13,12 @@ exports.run = (bot, message, args) => {
             data = bot.datatouse["kisses"].find(i => i["data"]["person-id"] == message.author.id);
         }
 
-        console.log(bot.datatouse["kisses"]);
-
         if (data) {
-            console.log('THIS PERSON DOES EXIST');
-
             //Update count
             data["data"]["number-of-kisses"] = data["data"]["number-of-kisses"] + 1;
             //Write to file
             fs.writeFileSync('./data-to-use.json', JSON.stringify(bot.datatouse));
-
-            console.log("added 1 to already counted player", bot.datatouse["kisses"]);
         } else {
-            console.log('THIS PERSON DOESN\'T EXIST');
-
             //Add to count
             bot.datatouse["kisses"].push({
                 "data": {
@@ -36,11 +28,25 @@ exports.run = (bot, message, args) => {
             });
             //Write to file
             fs.writeFileSync('./data-to-use.json', JSON.stringify(bot.datatouse));
-
-            console.log("added 1 to new counted player", bot.datatouse["kisses"]);
         }
 
-        console.log(bot.datatouse["kisses"]);
+        //Print out top leaderboard
+        var orderedArray = bot.datatouse["kisses"].sort((a, b) => b["number-of-kisses"] - a["number-of-kisses"]);
+        orderedArray = orderedArray.slice(0, 2);
+
+        var embeddedMessage = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setAuthor(bot.user.username, bot.user.avatarURL())
+            .setDescription(`${message.author.username} has kissed another person. Their score has increased!`)
+            .addFields(
+                { name: 'Top Kisser:', value: `${message.guild.memebers.cache.filter(i => i.user.id == orderedArray[0]["data"]["person-id"]).user} with ${orderedArray[0]["data"]["number-of-kisses"]} kisses!` },
+                { name: 'Runner Up Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[1]["data"]["person-id"]).user} with ${orderedArray[1]["data"]["number-of-kisses"]} kisses!` },
+                { name: 'Third Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[2]["data"]["person-id"]).user} with ${orderedArray[2]["data"]["number-of-kisses"]} kisses!` }
+            )
+            .setTimestamp()
+            .setFooter('Love you all :heart:!');
+        //send
+        message.channel.send(embeddedMessage);
 
         return true;
     }
