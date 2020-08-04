d2ad64d9ebabf256d03da1820e907a1a7e86599f
Arniox <nikkdiehl@gmail.com>
2020-08-04 20:54:44 +1200

    Fixed order and undefined variables of message

--- commands/kiss.js
+++ commands/kiss.js
@@ -34,23 +34,27 @@ exports.run = (bot, message, args) => {
         var orderedArray = bot.datatouse["kisses"].sort((a, b) => b["number-of-kisses"] - a["number-of-kisses"]);
         orderedArray = orderedArray.slice(0, 2);
 
-        var a, b, c = null;
-        if (orderedArray.length > 2)
-            c = { name: ':third_place: Third Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[2]["data"]["person-id"]).user} with ${orderedArray[2]["data"]["number-of-kisses"]} kisses!` };
-        else if (orderedArray.length > 1)
-            b = { name: ':second_place: Runner Up Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[1]["data"]["person-id"]).user} with ${orderedArray[1]["data"]["number-of-kisses"]} kisses!` };
-        else if (orderedArray.length > 0)
-            a = { name: ':first_place: Top Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[0]["data"]["person-id"]).user} with ${orderedArray[0]["data"]["number-of-kisses"]} kisses!` };
-
-        console.log(a, b, c);
-
+        //Create message
         var embeddedMessage = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setAuthor(bot.user.username, bot.user.avatarURL())
             .setDescription(`${message.author.username} has kissed another person. Their score has increased!`)
-            .addFields(a, b, c)
             .setTimestamp()
             .setFooter('Love you all :heart:!');
+
+        //Dynamically add fields
+        var a, b, c = null;
+        if (orderedArray.length > 0 && orderedArray.length < 2) {
+            a = { name: ':first_place: Top Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[0]["data"]["person-id"]).user} with ${orderedArray[0]["data"]["number-of-kisses"]} kisses!` };
+            embeddedMessage.addField(a);
+        } else if (orderedArray.length > 1 && orderedArray.length < 3) {
+            b = { name: ':second_place: Runner Up Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[1]["data"]["person-id"]).user} with ${orderedArray[1]["data"]["number-of-kisses"]} kisses!` };
+            embeddedMessage.addField(b);
+        } else if (orderedArray.length > 2 && orderedArray.length < 4) {
+            c = { name: ':third_place: Third Kisser:', value: `${message.guild.members.cache.filter(i => i.user.id == orderedArray[2]["data"]["person-id"]).user} with ${orderedArray[2]["data"]["number-of-kisses"]} kisses!` };
+            embeddedMessage.addField(c);
+        }
+
         //send
         message.channel.send(embeddedMessage);
 
