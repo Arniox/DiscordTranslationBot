e3a5950511c172af9cf0c18637743e471ce93b5d
Arniox <nikkdiehl@gmail.com>
2020-08-04 21:12:55 +1200

    Additional addFields. Not independent

--- commands/kiss.js
+++ commands/kiss.js
@@ -47,10 +47,12 @@ exports.run = (bot, message, args) => {
         if (orderedArray.length > 0 && orderedArray.length < 2) {
             a = { name: ':first_place: Top Kisser:', value: `${message.guild.members.cache.filter(i => i.id == orderedArray[0]["data"]["person-id"]).first().toString()} with ${orderedArray[0]["data"]["number-of-kisses"]} kisses!` };
             embeddedMessage.addFields(a);
-        } else if (orderedArray.length > 1 && orderedArray.length < 3) {
+        }
+        if (orderedArray.length > 1 && orderedArray.length < 3) {
             b = { name: ':second_place: Runner Up Kisser:', value: `${message.guild.members.cache.filter(i => i.id == orderedArray[1]["data"]["person-id"]).first().toString()} with ${orderedArray[1]["data"]["number-of-kisses"]} kisses!` };
             embeddedMessage.addFields(b);
-        } else if (orderedArray.length > 2 && orderedArray.length < 4) {
+        }
+        if (orderedArray.length > 2 && orderedArray.length < 4) {
             c = { name: ':third_place: Third Kisser:', value: `${message.guild.members.cache.filter(i => i.id == orderedArray[2]["data"]["person-id"]).first().toString()} with ${orderedArray[2]["data"]["number-of-kisses"]} kisses!` };
             embeddedMessage.addFields(c);
         }
