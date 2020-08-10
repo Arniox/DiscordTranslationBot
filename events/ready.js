4adb4fde1481803de3a520cd0ccf136c9e4b3b38
Arniox <nikkdiehl@gmail.com>
2020-08-11 02:40:54 +1200

    Added PrintSchedules command that checks the date every hour and only prints on sunday

--- events/ready.js
+++ events/ready.js
@@ -5,4 +5,18 @@ module.exports = (bot) => {
     console.log(`Logged in as ${bot.user.username} - (${bot.user.id})`);
     console.log(`Prefix: ${bot.config.prefix}`);
     console.log(`Prykie bancommand is: ${bot.config.bancommand.bancommand}`);
+
+    //Get print schedules command
+    const cmd = bot.commads.get("PrintSchedules");
+    //If command doesn't exist, exit and do nothing
+    if (!cmd) return;
+    else {
+        //Function variables / Globals
+        const minutes = 60, interval = minutes * 60 * 1000;
+        //Check schedules
+        setInterval(function () {
+            //Run command
+            cmd.run(bot);
+        }, interval);
+    }
 };
