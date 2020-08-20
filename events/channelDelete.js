91f22152cc5dc568f72bdc60044ab4725e1fe3ab
Arniox <nikkdiehl@gmail.com>
2020-08-20 22:36:45 +1200

    Removed logging for channelDelete event

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -3,22 +3,11 @@ const fs = require('fs');
 module.exports = (bot, channel) => {
     //Delete from config file
     var isChannel = bot.config.google["translate-ignored-channels"].find(i => i.id == channel.id);
-    console.log(isChannel);
-
     if (isChannel) {
         //Remove from array
         var index = bot.config.google["translate-ignored-channels"].indexOf(isChannel);
-
-        console.log(index);
-
-        console.log(bot.config.google["translate-ignored-channels"]);
-
         bot.config.google["translate-ignored-channels"].splice(index, 1);
-
-        console.log(bot.config.google["translate-ignored-channels"]);
-
+        //Save config
         fs.writeFileSync('./configure.json', JSON.stringify(bot.config));
-
-        console.log('Translation ignored channel was successfully deleted.');
     }
 };
