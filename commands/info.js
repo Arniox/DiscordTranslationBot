397fdf34f982e9efa23cf1de6e9752e6fa5a9e5e
Arniox <nikkdiehl@gmail.com>
2020-09-26 04:35:45 +1200

    Added heaps of commands for $info command
    $info
    
    $info servers count
    $info servers names
    
    $info total members
    $info total emojis
    $info total channels
    $info total voice :?channels
    $info total text :?channels
    $info total categories
    $info total news :?channels
    $info total store :?channels
    
    $info countme members
    $info countme emojis
    $info countme channels
    $info countme voice :?channels
    $info countme text :?channels
    $info countme categories
    $info countme news :?channels
    $info countme store :?channels
    
    $info list members
    $info list emojis
    $info list channels
    $info list voice :?channels
    $info list text :?channels
    $info list categories
    $info list news :?channels
    $info list store :?channels
    
    $info count messages [channel tag]

--- commands/info.js
+++ commands/info.js
@@ -16,23 +16,171 @@ exports.run = (bot, message, args) => {
                     //Switch on detail
                     switch (detail) {
                         case 'count':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`${bot.user.username} is part of ${bot.guilds.cache.size} discord servers.`).setColor('#0099ff'));
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Server Count:**\n\n${bot.user.username} is part of ${bot.guilds.cache.size} discord servers.`).setColor('#0099ff'));
                             break;
                         case 'names': case 'name':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`${bot.guilds.cache.map((value, key) => value.name).join(', ')}`).setColor('#0099ff'));
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of All Servers:**\n\n${bot.guilds.cache.map((value, key) => value.name).join(', ')}`).setColor('#0099ff'));
                             break;
                     }
                 } else {
-                    HelpMessage(bot, message, args);
+                    message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, you did not supply the detail arugment.').setColor('#b50909'));
                 }
                 break;
             case 'total':
+                if (args.length != 0) {
+                    //Get detail
+                    var detail = args.shift().toLowerCase();
+                    //Switch on detail
+                    switch (detail) {
+                        case 'members':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Member Count Accross All Servers:**\n\n${bot.users.cache.size}`).setColor('#0099ff'));
+                            break;
+                        case 'emojis':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Emoji Count Accross All Servers:**\n\n${bot.emojis.cache.size}`).setColor('#0099ff'));
+                            break;
+                        case 'channels':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Channel Count Accross All Servers:**\n\n${bot.channels.cache.size}`).setColor('#0099ff'));
+                            break;
+                        case 'voice':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Voice Channels Accross All Servers:**\n\n${bot.channels.cache.map((v, k) => v).filter(i => i.type == 'voice').length}`).setColor('#0099ff'));
+                            break;
+                        case 'text':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Text Channels Accross All Servers:**\n\n${bot.channels.cache.map((v, k) => v).filter(i => i.type == 'text').length}`).setColor('#0099ff'));
+                            break;
+                        case 'categories':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Categories Accross All Servers:**\n\n${bot.channels.cache.map((v, k) => v).filter(i => i.type == 'category').length}`).setColor('#0099ff'));
+                            break;
+                        case 'news':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total News Channels Accross All Servers:**\n\n${bot.channels.cache.map((v, k) => v).filter(i => i.type == 'news').length}`).setColor('#0099ff'));
+                            break;
+                        case 'store':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Store Channels Accross All Servers:**\n\n${bot.channels.cache.map((v, k) => v).filter(i => i.type == 'store').length}`).setColor('#0099ff'));
+                            break;
+                        default:
+                            message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, I did not understand the detail argument you provided.').setColor('#b50909'));
+                            break;
+                    }
+                } else {
+                    message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, you did not supply the detail argument.').setColor('#b50909'));
+                }
                 break;
             case 'countme':
+                if (args.length != 0) {
+                    //Get detail
+                    var detail = args.shift().toLowerCase();
+                    //Switch on detail
+                    switch (detail) {
+                        case 'members':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Member Count in this Discord:**\n\n${message.guild.memberCount}`).setColor('#0099ff'));
+                            break;
+                        case 'emojis':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Emoji Count in this Discord:**\n\n${message.guild.emojis.cache.size}`).setColor('#0099ff'));
+                            break;
+                        case 'channels':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Channel Count in this Discord:**\n\n${message.guild.channels.cache.size}`).setColor('#0099ff'));
+                            break;
+                        case 'voice':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Voice Channels in this Discord:**\n\n${message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'voice').length}`).setColor('#0099ff'));
+                            break;
+                        case 'text':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Text Channels in this Discord:**\n\n${message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'text').length}`).setColor('#0099ff'));
+                            break;
+                        case 'categories':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Categories in this Discord:**\n\n${message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'category').length}`).setColor('#0099ff'));
+                            break;
+                        case 'news':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total News Channels in this Discord:**\n\n${message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'news').length}`).setColor('#0099ff'));
+                            break;
+                        case 'store':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**Total Store Channels in this Discord:**\n\n${message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'store').length}`).setColor('#0099ff'));
+                            break;
+                        default:
+                            message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, I did not understand the detail argument you provided.').setColor('#b50909'));
+                            break;
+                    }
+                } else {
+                    message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, you did not supply the detail argument.').setColor('#b50909'));
+                }
                 break;
             case 'list':
+                if (args.length != 0) {
+                    //Get detail
+                    var detail = args.shift().toLowerCase();
+                    //Switch on detail
+                    switch (detail) {
+                        case 'members':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all Members in this Discord:**\n\n${cjoin(message.guild.members.cache.map((v, k) => v.user.username), ', ', 5, '\n')}`).setColor('#0099ff'));
+                            break;
+                        case 'emojis':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all Emojis in this Discord:**\n\n${cjoin(message.guild.emojis.cache.map((v, k) => v.toString()), ', ', 10, '\n')}`).setColor('#0099ff'));
+                            break;
+                        case 'channels':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all Channels in this Discord:**\n\n${cjoin(message.guild.channels.cache.map((v, k) => v.toString()), ', ', 5, '\n')}`).setColor('#0099ff'));
+                            break;
+                        case 'voice':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all Voice Channels in this Discord:**\n\n` +
+                                `${cjoin(message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'voice').map(v => v.toString()), ', ', 5, '\n')}`).setColor('#0099ff'));
+                            break;
+                        case 'text':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all Text Channels in this Discord:**\n\n` +
+                                `${cjoin(message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'text').map(v => v.toString()), ', ', 5, '\n')}`).setColor('#0099ff'));
+                            break;
+                        case 'categories':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all Categories in this Discord:**\n\n` +
+                                `${cjoin(message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'categories').map(v => v.toString()), ', ', 5, '\n')}`).setColor('#0099ff'));
+                            break;
+                        case 'news':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all News Channels in this Discord:**\n\n` +
+                                `${cjoin(message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'news').map(v => v.toString()), ', ', 5, '\n')}`).setColor('#0099ff'));
+                            break;
+                        case 'store':
+                            message.channel.send(new Discord.MessageEmbed().setDescription(`**List of all Store Channels in this Discord:**\n\n` +
+                                `${cjoin(message.guild.channels.cache.map((v, k) => v).filter(i => i.type == 'store').map(v => v.toString()), ', ', 5, '\n')}`).setColor('#0099ff'));
+                            break;
+                        default:
+                            message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, I did not understand the detail argument you provided.').setColor('#b50909'));
+                            break;
+                    }
+                } else {
+                    message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, you did not supply the detail argument.').setColor('#b50909'));
+                }
                 break;
             case 'count':
+                if (args.length != 0) {
+                    //Check that you mentioned a channel
+                    if (channelMentions.size != 0) {
+                        //Check that you only mentioned one channel
+                        if (channelMentions.size < 2) {
+                            //Get detail
+                            var detail = args.shift().toLowerCase();
+                            //Switch on detail
+                            switch (detail) {
+                                case 'messages':
+                                    //Send loading message.
+                                    message.channel
+                                        .send(new Discord.MessageEmbed().setDescription(`**Total Messages in ${channelMentions.first().toString()}:**\n\n***Loading....***`).setColor('#FFCC00'))
+                                        .then((sent) => {
+                                            //Fetch all messages
+                                            channelMentions.first().messages.fetch()
+                                                .then((messages) => {
+                                                    sent.edit(new Discord.MessageEmbed().setDescription(`**Total Messages in ${channelMentions.first().toString()}:**\n\n${messages.size}`).setColor('#b50909'))
+                                                })
+                                                .catch((err) => console.log(err));
+                                        });
+                                    break;
+                                default:
+                                    message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, I did not understand the detail argument you provided.').setColor('#b50909'));
+                                    break;
+                            }
+                        } else {
+                            message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, I cannot count from more than one channel.').setColor('#b50909'));
+                        }
+                    } else {
+                        message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, you didn\'t mention a channel for me to check.').setColor('#b50909'));
+                    }
+                } else {
+                    message.channel.send(new Discord.MessageEmbed().setDescription('Sorry, you did not supply the detail arugment.').setColor('#b50909'));
+                }
                 break;
             default:
                 HelpMessage(bot, message, args);
@@ -98,4 +246,18 @@ function HelpMessage(bot, message, args) {
 
     //set embedded message
     message.channel.send(embeddedHelpMessage);
+}
+
+//Custom join
+function cjoin(array, seperator = '', splittingDistance = 0, splittingSeperator = '') {
+    //If there is no splitting distance then just returned joined array
+    if (splittingDistance == 0) return array.join(seperator);
+    else {
+        var out = '';
+        for (var i = 0; i < array.length; i++) {
+            if (i % splittingDistance == 0) out += splittingSeperator + array[i];
+            else out += array[i];
+        }
+        return array;
+    }
 }
