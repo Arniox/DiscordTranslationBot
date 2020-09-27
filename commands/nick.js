5c573cad87b0b615b2f5bac9ba2d71fd747a9167
Arniox <nikkdiehl@gmail.com>
2020-09-28 06:54:30 +1300

    test

--- commands/nick.js
+++ commands/nick.js
@@ -77,6 +77,7 @@ exports.run = (bot, guild, message, args) => {
                                                             ` nickname due to Missing Permissions`).setColor('#b50909'));
                                                     }
                                                 });
+                                                return sent;
                                             }).then((sent) => {
                                                 sent.edit(new Discord.MessageEmbed().setDescription(`✅ Translated ${members.size} / ${members.size} members nicknames into ${value.name}`).setColor('#09b50c'));
                                             });
