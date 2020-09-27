c5ea496959785c5a4eee3244b3548e4a4d26637a
Arniox <nikkdiehl@gmail.com>
2020-09-28 00:33:42 +1300

    Added mysql connection promise function

--- bot.js
+++ bot.js
@@ -8,72 +8,71 @@ var settings = require('./configure.json');
 var dataToUse = require('./data-to-use.json');
 var fs = require('fs');
 
+//Create mysql connection pool
 const pool = mysql.createPool({
-	host: process.env.CLEARDB_DATABASE_URL.match(/(@)[a-zA-Z\d-.]+/).join('').replace(/(@)/, ''), //Get host
-	user: process.env.CLEARDB_DATABASE_URL.match(/(\/\/)[a-zA-Z\d]+/).join('').replace(/(\/\/)/, ''), //Get user
-	password: process.env.CLEARDB_DATABASE_URL.match(/(:)[a-zA-Z\d]+/).join('').replace(/(:)/, ''), //Get password
-	database: process.env.CLEARDB_DATABASE_URL.match(/([a-zA-Z]\/)[a-zA-Z\d_]+/).join('').replace(/([a-zA-Z]\/)/, '') //Get database
+	host: process.env.CLEARDB_DATABASE_URL.match(/(@)[a-zA-Z\d-.]+/)[0].replace(/(@)/, ''), //Get host
+	user: process.env.CLEARDB_DATABASE_URL.match(/(\/\/)[a-zA-Z\d]+/)[0].replace(/(\/\/)/, ''), //Get user
+	password: process.env.CLEARDB_DATABASE_URL.match(/(:)[a-zA-Z\d]+/)[0].replace(/(:)/, ''), //Get password
+	database: process.env.CLEARDB_DATABASE_URL.match(/([a-zA-Z]\/)[a-zA-Z\d_]+/)[0].replace(/([a-zA-Z]\/)/, '') //Get database
 });
-
-console.log(process.env.CLEARDB_DATABASE_URL.match(/(@)[a-zA-Z\d-.]+/)[0].replace(/(@)/, ''));
-console.log(process.env.CLEARDB_DATABASE_URL.match(/(\/\/)[a-zA-Z\d]+/)[0].replace(/(\/\/)/, ''));
-console.log(process.env.CLEARDB_DATABASE_URL.match(/(:)[a-zA-Z\d]+/)[0].replace(/(:)/, ''));
-console.log(process.env.CLEARDB_DATABASE_URL.match(/([a-zA-Z]\/)[a-zA-Z\d_]+/)[0].replace(/([a-zA-Z]\/)/, ''));
 
 //Initialize Discord bot 
 var bot = new Discord.Client();
-//Randomise prykie ban command
-settings.bancommand.bancommand = CreateCommand(10);
-//Write to file
-fs.writeFileSync('./configure.json', JSON.stringify(settings));
-//Attach settings to bot
-bot.config = settings;
-bot.datatouse = dataToUse;
 
-//Attaches all event files to event
-fs.readdir('./events/', (err, files) => {
-	if (err)
-		return console.error(err); //Throw if file breaks
-	files.forEach(file => {
-		//if file is not js file, ignore
-		if (!file.endsWith(".js"))
-			return;
-		//Load event file
-		const event = require(`./events/${file}`);
-		//Get the event name from file nmae
-		var eventName = file.split('.')[0];
-		console.log(`Attempting to load event ${eventName}`);
-		//Bind all found events to client
-		bot.on(eventName, event.bind(null, bot));
-		delete require.cache[require.resolve(`./events/${file}`)]; //Remove from memory
-	});
-});
+pool.then((p) => {
+	return p.getConnection();
+}).then((connection) => {
+	console.error(err, `Connection to ${process.env.CLEARDB_DATABASE_URL} has failed!`);
+	bot.con = connection; //Attach connection to bot
 
-//Set up commands and attach to the client
-bot.commands = new Enmap();
-fs.readdir('./commands/', (err, files) => {
-	if (err)
-		return console.error(err); //Throw if folder breaks
-	files.forEach(file => {
-		//if file is not js file, ignore
-		if (!file.endsWith(".js"))
-			return;
-		//Load the command file itself
-		var props = require(`./commands/${file}`);
-		//Get just the command name from the file name
-		var commandName = file.split('.')[0];
-		console.log(`Attempting to load command ${commandName}`);
-		//Store the command in the command Enmap.
-		bot.commands.set(commandName, props);
+	//Randomise prykie ban command
+	settings.bancommand.bancommand = CreateCommand(10);
+	//Write to file
+	fs.writeFileSync('./configure.json', JSON.stringify(settings));
+	//Attach settings to bot
+	bot.config = settings;
+	bot.datatouse = dataToUse;
+
+	//Attaches all event files to event
+	fs.readdir('./events/', (err, files) => {
+		if (err) return console.error(err); //Throw if file breaks
+		files.forEach(file => {
+			//if file is not js file, ignore
+			if (!file.endsWith(".js")) return;
+			//Load event file
+			const event = require(`./events/${file}`);
+			//Get the event name from file nmae
+			var eventName = file.split('.')[0];
+			console.log(`Attempting to load event ${eventName}`);
+			//Bind all found events to client
+			bot.on(eventName, event.bind(null, bot));
+			delete require.cache[require.resolve(`./events/${file}`)]; //Remove from memory
+		});
 	});
+
+	//Set up commands and attach to the client
+	bot.commands = new Enmap();
+	fs.readdir('./commands/', (err, files) => {
+		if (err) return console.error(err); //Throw if folder breaks
+		files.forEach(file => {
+			//if file is not js file, ignore
+			if (!file.endsWith(".js")) return;
+			//Load the command file itself
+			var props = require(`./commands/${file}`);
+			//Get just the command name from the file name
+			var commandName = file.split('.')[0];
+			console.log(`Attempting to load command ${commandName}`);
+			//Store the command in the command Enmap.
+			bot.commands.set(commandName, props);
+		});
+	});
+	//Handle promise rejections
+	process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+}).catch(err => {
+	console.error(err, `Connection to ${process.env.CLEARDB_DATABASE_URL} has failed!`);
 });
-//Handle promise rejections
-process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
 //Bot loggin
 bot.login(process.env.BOT_TOKEN);
-
-
-
 
 //----------------FUNCTIONS--------------------------------
 //Function variables / Globals
@@ -82,8 +81,6 @@ const minutes = 20, interval = minutes * 60 * 1000;
 setInterval(function () {
 	console.log('I am currently alive.');
 }, interval);
-
-
 
 //Get random string of length
 function CreateCommand(length) {
