6e21285508adad545eb0da14d9a8293e411ad6fb
Arniox <nikkdiehl@gmail.com>
2020-09-28 00:26:36 +1300

    logging

--- bot.js
+++ bot.js
@@ -15,10 +15,10 @@ const pool = mysql.createPool({
 	database: process.env.CLEARDB_DATABASE_URL.match(/([a-zA-Z]\/)[a-zA-Z\d_]+/).join('').replace(/([a-zA-Z]\/)/, '') //Get database
 });
 
-console.log(process.env.CLEARDB_DATABASE_URL.match(/(@)[a-zA-Z\d-.]+/));
-console.log(process.env.CLEARDB_DATABASE_URL.match(/(\/\/)[a-zA-Z\d]+/));
-console.log(process.env.CLEARDB_DATABASE_URL.match(/(:)[a-zA-Z\d]+/));
-console.log(process.env.CLEARDB_DATABASE_URL.match(/([a-zA-Z]\/)[a-zA-Z\d_]+/));
+console.log(process.env.CLEARDB_DATABASE_URL.match(/(@)[a-zA-Z\d-.]+/)[0].replace(/(@)/, ''));
+console.log(process.env.CLEARDB_DATABASE_URL.match(/(\/\/)[a-zA-Z\d]+/)[0].replace(/(\/\/)/, ''));
+console.log(process.env.CLEARDB_DATABASE_URL.match(/(:)[a-zA-Z\d]+/)[0].replace(/(:)/, ''));
+console.log(process.env.CLEARDB_DATABASE_URL.match(/([a-zA-Z]\/)[a-zA-Z\d_]+/)[0].replace(/([a-zA-Z]\/)/, ''));
 
 //Initialize Discord bot 
 var bot = new Discord.Client();
