c9e051a4d77e85f43c21eed14455ad9bd1b2c649
Arniox <nikkdiehl@gmail.com>
2020-09-28 04:36:47 +1300

    fixed a main crash in guildCreate.js event

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,10 +2,10 @@
 const mysql = require('promise-mysql');
 
 module.exports = (bot, guild) => {
-    console.log(`Joined a new guild: ` + guild.name);
+    console.log(`Joined a new guild: ${guild.name}`);
 
     //Check if guild exists in database already
-    const sql_cmd = `SELECT * FROM configure WHERE ServerId = ${guild.id}`;
+    const sql_cmd = `SELECT * FROM servers WHERE ServerId = ${guild.id}`;
     bot.con.query(sql_cmd, (error, results, fields) => {
         if (error) return console.error(error); //Return error console log and return
         else if (!results || !results.length) {
