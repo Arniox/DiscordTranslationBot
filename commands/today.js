64cae847f6724308981678854a07c6816b345ee9
Arniox <nikkdiehl@gmail.com>
2020-10-04 21:45:36 +1300

    Updated today command to use the new moment.js library

--- commands/today.js
+++ commands/today.js
@@ -7,130 +7,119 @@ exports.run = (bot, guild, message, args) => {
         var command = args.shift().toLowerCase();
         //Get now
         var now = moment().tz('Australia/Sydney');
-        console.log(now.format());
 
         //Switch on today command
         switch (command) {
-            case 'get':
-                if (args.length != 0) {
-                    //Get the user selected range
-                    var range = args.map(i => i.toLowerCase()).join('');
-                    switch (range) {
-                        case 'y': case 'year': case 'years':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`Today's full year is: ${new Date().toTimeZone(guild.Time_Zone_Offset).getFullYear()}.`).setColor('#09b50c'));
-                            break;
-                        case 'month': case 'months':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`Today's month is the: ${(new Date().toTimeZone(guild.Time_Zone_Offset).getMonth() + 1).ordinal()} month of the year.`).setColor('#09b50c'));
-                            break;
-                        case 'date':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`Today's date is the: ${(new Date().toTimeZone(guild.Time_Zone_Offset).getDate()).ordinal()}.`).setColor('#09b50c'));
-                            break;
-                        case 'day': case 'days':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`Today is the : ${(new Date().toTimeZone(guild.Time_Zone_Offset).getDay() + 1).ordinal()} day of the week.`).setColor('#09b50c'));
-                            break;
-                        case 'h': case 'hour': case 'hours':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`It is ${new Date().toTimeZone(guild.Time_Zone_Offset).getHours().toString().padEnd(3, '0').padStart(4, '0')} hours.`).setColor('#09b50c'));
-                            break;
-                        case 'm': case 'min': case 'mins': case 'minute': case 'minutes':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`It is ${new Date().toTimeZone(guild.Time_Zone_Offset).getMinutes().toString().padStart(4, '0')} minutes.`).setColor('#09b50c'));
-                            break;
-                        case 's': case 'sec': case 'secs': case 'second': case 'seconds':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`It is ${new Date().toTimeZone(guild.Time_Zone_Offset).getSeconds()} seconds right now.`).setColor('#09b50c'));
-                            break;
-                        case 'mill': case 'milli': case 'millisecond': case 'milliseconds':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`It is ${new Date().toTimeZone(guild.Time_Zone_Offset).getMilliseconds()} milliseconds right now.`).setColor('#09b50c'));
-                            break;
-                        case 'time':
-                            message.channel.send(
-                                new Discord.MessageEmbed()
-                                    .setDescription(`The time in milliseconds is ${new Date().toTimeZone(guild.Time_Zone_Offset).getTime()}.`)
-                                    .addField('Fact: ', 'This value in programming is the number of milliseconds after the incredably specific date of Jan 1, 1970, 00:00:00.000 GMT').setColor('#09b50c'));
-                            break;
-                        case 'timezoneoffset': case 'timezoneoff': case 'timezone': case 'zone':
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`Your timezone offset is ${new Date().toTimeZone(guild.Time_Zone_Offset).getTimezoneOffset()} `).setColor('#09b50c'));
-                            break;
-                        default:
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, you need to select something to get from today's date.`).setColor('#b50909'));
-                            break;
-                    }
-                } else {
-                    message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, you need to select something to get from today's date.`).setColor('#b50909'));
-                }
+            //Get time sections
+            case 'eras': case 'era': case 'e':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Era: **${now.format('N')}**, **${now.format('NNNN')}**`).setColor('#09b50c'));
                 break;
-            case 'now':
+            case 'years': case 'year': case 'y':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Year: **${now.format('YYYY')}**`).setColor('#09b50c'));
+                break;
+            case 'quarters': case 'quarter': case 'q':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Yearly Quarter: **${now.format('Qo')}**`).setColor('#09b50c'));
+            case 'months': case 'month': case 'm':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Month: **${now.format('MMMM')}**, the ${now.format('Mo')} Month of the Year`).setColor('#09b50c'));
+                break;
+            case 'weeks': case 'week': case 'w':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Week of the Year: **${now.format('wo')}**`).setColor('#09b50c'));
+                break;
+            case 'days': case 'day': case 'd':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Date is the **${now.format('Do')}**; **${now.format('DDDo')}** of the Year - **${now.format('dddd')}**`).setColor('#09b50c'));
+                break;
+            case 'hours': case 'hour': case 'h':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Hour: **${now.format('HH')}**`).setColor('#09b50c'));
+                break;
+            case 'minutes': case 'minute': case 'min': case 'mm':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Minute: **${now.format('mm')}**`).setColor('#09b50c'));
+                break;
+            case 'seconds': case 'second': case 'sec': case 's':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Second: **${now.format('ss')}**`).setColor('#09b50c'));
+                break;
+            case 'milliseconds': case 'millisecond': case 'milli': case 'mill': case 'mmm': case 'mi':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Millisecond: **${now.format('SSS')}**`).setColor('#09b50c'));
+                break;
+            //Get extra details
+            case 'time': case 't':
+                message.channel.send(new Discord.MessageEmbed().setDescription(`Current Date & Time in Milliseconds is **${now.format('x')}**`).setColor('#09b50c'));
+                break;
+            case 'now': case 'n':
                 //Get the user selected range
                 var range = args.map(i => i.toLowerCase()).join('');
                 switch (range) {
-                    case 'short':
-                        message.channel.send(new Discord.MessageEmbed().setDescription(`The local date & time right now is: ${new Date().toTimeZone(guild.Time_Zone_Offset).toDateString()}`).setColor('#09b50c'));
+                    case 'short': case 's':
+                        message.channel.send(new Discord.MessageEmbed().setDescription(`Today is: **${now.format('LTS - L z | ZZ')}**`).setColor('#09b50c'));
                         break;
-                    case 'long':
-                        message.channel.send(new Discord.MessageEmbed().setDescription(`The local date & time right now is: ${new Date().toTimeZone(guild.Time_Zone_Offset).toString()}`).setColor('#09b50c'));
+                    case 'long': case 'l':
+                        message.channel.send(new Discord.MessageEmbed().setDescription(`Today is: **${now.format('LLLL zz | ZZ')}**`).setColor('#09b50c'));
                         break;
                     default:
-                        message.channel.send(new Discord.MessageEmbed().setDescription(`The local date & time right now is: ${new Date().toTimeZone(guild.Time_Zone_Offset).toLocaleString('en-NZ')}`).setColor('#09b50c'));
+                        message.channel.send(new Discord.MessageEmbed().setDescription(`Today is: **${now.format('lll z')}**`).setColor('#09b50c'));
                         break;
                 }
                 break;
-            case 'utc':
-                message.channel.send(new Discord.MessageEmbed().setDescription(`The date & time GMT 00 is: ${new Date().toTimeZone(guild.Time_Zone_Offset).toUTCString()}`).setColor('#09b50c'));
-                break;
-            case 'iso':
-                message.channel.send(new Discord.MessageEmbed().setDescription(`The ISO date & time is: ${new Date().toTimeZone(guild.Time_Zone_Offset).toISOString()}`).setColor('#09b50c'));
-                break;
             default:
+                //Check if the user wants to add time
                 if (args.length != 0) {
-                    //get the left over strings
-                    var strings = args.shift().match(/[a-z]+|[^a-z]+/gi);
-                    if (strings.length >= 2) {
-                        //get the number
-                        var number = strings.shift();
-                        if (/^\d+$/.test(number)) {
-                            //get digit range
-                            var digitRange = strings.shift();
+                    //Get the left over strings
+                    var strings = args.shift().match(/[+-][\d]+[a-z]{1,2}/gi);
+                    if (strings.length > 0) {
+                        //For each of the strings
+                        var rightNow = moment().tz('Australia/Sydney');
 
-                            //Create key value list
-                            var usefulDigits = {
-                                'y': { 'name': 'year', 'value': 31556952000 },
-                                'M': { 'name': 'month', 'value': 2592000000 },
-                                'd': { 'name': 'day', 'value': 86400000 },
-                                'h': { 'name': 'hour', 'value': 3600000 },
-                                'm': { 'name': 'minute', 'value': 60000 },
-                                's': { 'name': 'second', 'value': 1000 },
-                                'ms': { 'name': 'millisecond', 'value': 1 }
-                            };
+                        //Foreach
+                        strings.forEach(e => {
+                            //Get ID
+                            var identifier = e.match(/[+-]|[a-z]+|[^a-z]+/gi);
+                            //Get the Mathematical Sign
+                            var sign = identifier.shift();
+                            //Get the number
+                            var number = identifier.shift();
+                            if (/^\d+$/.test(number)) {
+                                //Get digit range
+                                var digitRange = identifier.shift();
 
-                            //Get today
-                            var now = new Date().toTimeZone(guild.Time_Zone_Offset);
+                                //Create key value list
+                                var usefulDigits = {
+                                    'y': { 'name': 'year' },
+                                    'Q': { 'name': 'quarter' },
+                                    'M': { 'name': 'month' },
+                                    'w': { 'name': 'week' },
+                                    'd': { 'name': 'day' },
+                                    'h': { 'name': 'hour' },
+                                    'm': { 'name': 'minute' },
+                                    's': { 'name': 'second' },
+                                    'ms': { 'name': 'millisecond' }
+                                };
 
-                            if (digitRange in usefulDigits) {
-                                //Switch on the mathematical method
-                                switch (command) {
-                                    case '+':
-                                        message.channel.send(
-                                            new Discord.MessageEmbed()
-                                                .setDescription(`Today (${now.toDateString()})\n+${number} ${usefulDigits[digitRange]["name"]}(s) is: \n${new Date(now.getTime() + (usefulDigits[digitRange]["value"] * parseInt(number))).toString()}`)
-                                                .setColor('#09b50c'));
-                                        break;
-                                    case '-':
-                                        message.channel.send(
-                                            new Discord.MessageEmbed()
-                                                .setDescription(`Today (${now.toDateString()})\n-${number} ${usefulDigits[digitRange]["name"]}(s) is: \n${new Date(now.getTime() - (usefulDigits[digitRange]["value"] * parseInt(number))).toString()}`)
-                                                .setColor('#09b50c'));
-                                        break;
-                                    default:
-                                        message.channel.send(
-                                            new Discord.MessageEmbed()
-                                                .setDescription(`Sorry, the mathematical method you selected is not possible right now.`).setColor('#b50909'));
-                                        break;
+                                //Check if exists
+                                if (digitRange in usefulDigits) {
+                                    //Switch on mathematical method
+                                    switch (sign) {
+                                        case '+':
+                                            rightNow.add(number, digitRange);
+                                            break;
+                                        case '-':
+                                            rightNow.subtract(number, digitRange);
+                                            break;
+                                        default:
+                                            message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, ${sign} is not a supported math method right now.`).setColor('#b50909'));
+                                            break;
+                                    }
+                                } else {
+                                    message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, ${digitRange} was not a date type I understood.\n` +
+                                        `You can apply years (y), months (M), days (d), hours (h), minutes (m), seconds (s), and milliseconds (ms)`).setColor('#b50909'));
                                 }
                             } else {
-                                message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, ${digitRange} was not a date type I understood.\n` +
-                                    `You can apply years (y), months (M), days (d), hours (h), minutes (m), seconds (s), and milliseconds (ms)`).setColor('#b50909'));
+                                message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, ${number} was not a number I understood.`).setColor('#b50909'));
                             }
-                        } else {
-                            message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, ${number} was not a number I understood.`).setColor('#b50909'));
-                        }
+                        });
+
+                        //Print
+                        message.channel.send(new Discord.MessageEmbed().setDescription(`Today **(${now.format('llll')})**\n` +
+                            `${strings.map(i => `${i.match(/[+-]/gi)[0]} ${i.match(/[+-]+/gi)[0]} ${usefulDigits[i.match(/[a-z]+/gi)[0]]["name"]}`).join('\n')} is:\n` +
+                            `**${rightNow.format('llll')}**`).setColor('#b50909'));
                     } else {
                         message.channel.send(new Discord.MessageEmbed().setDescription(`Sorry, I did not understand the time range you wanted.`).setColor('#b50909'));
                     }
