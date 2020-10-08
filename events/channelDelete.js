895a04953bb67303e418744741906303bf1797cb
Arniox <nikkdiehl@gmail.com>
2020-10-08 23:43:29 +1300

    channelDelete event now effects the database rather than the depricated file system

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,13 +1,25 @@
 const fs = require('fs');
 
 module.exports = (bot, channel) => {
-    //Delete from config file
-    var isChannel = bot.config.google["translate-ignored-channels"].find(i => i.id == channel.id);
-    if (isChannel) {
-        //Remove from array
-        var index = bot.config.google["translate-ignored-channels"].indexOf(isChannel);
-        bot.config.google["translate-ignored-channels"].splice(index, 1);
-        //Save config
-        fs.writeFileSync('./configure.json', JSON.stringify(bot.config));
-    }
+    //Delete from database
+    const sql_cmd = `
+    SELECT * FROM translation_ignored_channels
+        WHERE ServerId = "${channel.guild.id}"
+    `;
+    bot.con.query(sql_cmd, (error, results, fields) => {
+        if (error) return console.error(error); //Return error console log
+
+        //Check if exists in the database
+        if (results.map(v => v.ChannelId).includes(c.id)) {
+            //Remove
+            const remove_sql = `
+            DELETE FROM translation_ignored_channels
+                WHERE ChannelId = "${c.id}"
+                AND ServerId = "${channel.guild.id}"
+            `;
+            bot.con.query(remove_sql, (error, results, fields) => {
+                if (error) return console.error(error); //Return error console log
+            });
+        }
+    });
 };
