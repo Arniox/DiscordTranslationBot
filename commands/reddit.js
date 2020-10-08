5d823197c3b5fe60b26f578baa324d0358dcb1f3
Arniox <nikkdiehl@gmail.com>
2020-10-08 17:03:36 +1300

    Removed reddit-wrapper-v2 as it was old and probably depricated. Added reddit and js-reddit.js

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,29 +1,18 @@
 //Import
 const Discord = require('discord.js');
-const RedditAPI = require('reddit-wrapper-v2');
+const Reddit = require('js-reddit.js');
 
-const reddit = new RedditAPI({
-    //Options for Reddit Wrapper
+//Set up reddit options
+const opts = {
     username: process.env.REDDIT_USERNAME,
     password: process.env.REDDIT_PASSWORD,
-    app_id: process.env.REDDIT_APP_ID,
-    api_secret: process.env.REDDIT_APP_SECRET,
-    user_agent: `PC:https://discordapp.com/api/oauth2/authorize?client_id=707436231222493214&permissions=133160&scope=bot` +
-        ` v${process.env.HEROKU_RELEASE_VERSION} (by /u/-arniox-)`,
-    retry_on_wait: true,
-    retry_on_server_error: 5,
-    logs: true
-});
+    appId: process.env.REDDIT_APP_ID,
+    appSecret: process.env.REDDIT_APP_SECRET,
+    userAgent: `PC:https://discordapp.com/api/oauth2/authorize?client_id=707436231222493214&permissions=133160&scope=bot` +
+        ` v${process.env.HEROKU_RELEASE_VERSION} (by /u/-arniox-)`
+}
 
 exports.run = (bot, guild, message, args) => {
     //Test
-    reddit.api.get('/api/v1/me/karma/')
-        .then((res) => {
-            let responseCode = res[0];
-            let responseData = res[1];
 
-            console.log(`Received response (${responseCode}): ${responseData}`);
-        }).catch((err) => {
-            return console.error(`api request failed: ${err}`);
-        });
 }
