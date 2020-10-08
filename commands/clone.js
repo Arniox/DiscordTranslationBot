3f10b400cbfd814b0a03eefd19bd7e38dc9adc99
Arniox <nikkdiehl@gmail.com>
2020-10-09 00:45:57 +1300

    Changed from Nullish coalescing operator (??) to Logical nullish assignment (??=)

--- commands/clone.js
+++ commands/clone.js
@@ -101,7 +101,8 @@ async function cloneCountSequentially(thisChannel, toChannel, message, flags) {
                     .setTimestamp(v.createdAt)
                 ).catch(() => { return; });
                 //Send links seperately with match link regex
-                var linkArray = v.content.match(/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm) ?? [];
+                var linkArray = v.content.match(/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm);
+                linkArray ??= []; //Set to empty array if linkArray is nullish
                 for (var i = 0; i < linkArray.length; i++) await toChannel.send(link);
             }
 
