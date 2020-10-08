234c7c2822cd5fc570daab2c3e3503114bb28138
Arniox <nikkdiehl@gmail.com>
2020-10-09 00:52:54 +1300

    loop properly without missing variable

--- commands/clone.js
+++ commands/clone.js
@@ -102,7 +102,7 @@ async function cloneCountSequentially(thisChannel, toChannel, message, flags) {
                 ).catch(() => { return; });
                 //Send links seperately with match link regex
                 var linkArray = v.content.match(/(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm);
-                for (var i = 0; i < (linkArray ? linkArray : []).length; i++) await toChannel.send(link);
+                for (var i = 0; i < (linkArray ? linkArray : []).length; i++) await toChannel.send(linkArray[i]);
             }
 
             //Delete all 100 messages
