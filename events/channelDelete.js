b9d70bae139a17b2284b1f53cc0643241d7fa6e8
Arniox <nikkdiehl@gmail.com>
2020-10-09 00:08:27 +1300

    channelDelete will now properly create a new connection to the database pool and then release the connection when done

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,25 +1,39 @@
-const fs = require('fs');
+//Import
+const mysql = require('promise-mysql');
 
 module.exports = (bot, channel) => {
-    //Delete from database
-    const sql_cmd = `
-    SELECT * FROM translation_ignored_channels
-        WHERE ServerId = "${channel.guild.id}"
-    `;
-    bot.con.query(sql_cmd, (error, results, fields) => {
-        if (error) return console.error(error); //Return error console log
+    //Get connection
+    bot.dbpool.then((p) => {
+        return p.getConnection();
+    }).then((connection) => {
+        //Attach connection to bot
+        bot.con = connection;
 
-        //Check if exists in the database
-        if (results.map(v => v.ChannelId).includes(channel.id)) {
-            //Remove
-            const remove_sql = `
-            DELETE FROM translation_ignored_channels
-                WHERE ChannelId = "${channel.id}"
-                AND ServerId = "${channel.guild.id}"
-            `;
-            bot.con.query(remove_sql, (error, results, fields) => {
-                if (error) return console.error(error); //Return error console log
-            });
-        }
+        //Delete from database
+        const sql_cmd = `
+        SELECT * FROM translation_ignored_channels
+            WHERE ServerId = "${channel.guild.id}"
+        `;
+        bot.con.query(sql_cmd, (error, results, fields) => {
+            if (error) return console.error(error); //Return error console log
+
+            //Check if exists in the database
+            if (results.map(v => v.ChannelId).includes(channel.id)) {
+                //Remove
+                const remove_sql = `
+                DELETE FROM translation_ignored_channels
+                    WHERE ChannelId = "${channel.id}"
+                    AND ServerId = "${channel.guild.id}"
+                `;
+                bot.con.query(remove_sql, (error, results, fields) => {
+                    if (error) return console.error(error); //Return error console log
+                });
+            }
+        });
+
+        //Release connection when done
+        bot.con.release();
+    }).catch((err) => {
+        console.log(err, `Connection failed on channelDelete`);
     });
-};
+}
