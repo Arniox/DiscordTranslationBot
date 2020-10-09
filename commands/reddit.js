250a8a3268a758e9630e706f00424d092de23782
Arniox <nikkdiehl@gmail.com>
2020-10-09 19:26:05 +1300

    log testing

--- commands/reddit.js
+++ commands/reddit.js
@@ -3,12 +3,13 @@ const Discord = require('discord.js');
 
 exports.run = (bot, guild, message, args) => {
     //Test
-    new Promise(async (resolve, reject) => {
-        await resolve(bot.reddit.get('/api/v1/me'));
-    }).then((res) => {
-        console.log(res);
-    }).catch((err) => {
-        console.error(err);
-    })
+    bot.reddit.fetchSelf()
+        .then(user => {
+            console.log(`This client is logged into ${user.name}`);
+        }).catch((err) => {
+            console.log('THIS IS GAY');
+
+            console.error(err);
+        });
 
 }
