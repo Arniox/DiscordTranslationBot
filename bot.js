cd258e59f3500a3c6fdcebcf3dfa7c18eb0a33d2
Arniox <nikkdiehl@gmail.com>
2020-10-09 19:28:06 +1300

    remove userAgent

--- bot.js
+++ bot.js
@@ -18,18 +18,14 @@ const reddit = new RedditJS.Client({
 	username: process.env.REDDIT_USERNAME,
 	password: process.env.REDDIT_PASSWORD,
 	appId: process.env.REDDIT_APP_ID,
-	appSecret: process.env.REDDIT_APP_SECRET,
-	userAgent: `PC:https://discordapp.com/api/oauth2/authorize?client_id=707436231222493214&permissions=133160&scope=bot` +
-		` v${process.env.HEROKU_RELEASE_VERSION} (by /u/-arniox-)`
+	appSecret: process.env.REDDIT_APP_SECRET
 });
 
 console.log({
 	username: process.env.REDDIT_USERNAME,
 	password: process.env.REDDIT_PASSWORD,
 	appId: process.env.REDDIT_APP_ID,
-	appSecret: process.env.REDDIT_APP_SECRET,
-	userAgent: `PC:https://discordapp.com/api/oauth2/authorize?client_id=707436231222493214&permissions=133160&scope=bot` +
-		` v${process.env.HEROKU_RELEASE_VERSION} (by /u/-arniox-)`
+	appSecret: process.env.REDDIT_APP_SECRET
 });
 console.log(reddit);
 
