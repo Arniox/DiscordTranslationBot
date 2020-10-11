b0c2a93db3583c2dbe757878bbaf7b06702b6729
Arniox <nikkdiehl@gmail.com>
2020-10-11 22:17:18 +1300

    fixed issue

--- commands/reddit.js
+++ commands/reddit.js
@@ -45,7 +45,7 @@ exports.run = (bot, guild, message, args) => {
                                 //If sub flairs are enabled
                                 if (subFlairs && subFlairs.length > 0) {
                                     //Get emojis in loop to add to the number of flairs
-                                    var emojis = randomEmoji.random({ count: subFlairs.length }).map(i, index => { return { "emoji": i.character, "flairIndex": index }; });
+                                    var emojis = randomEmoji.random({ count: subFlairs.length }).map((i, index) => { return { "emoji": i.character, "flairIndex": index }; });
 
                                     console.log(emojis);
 
