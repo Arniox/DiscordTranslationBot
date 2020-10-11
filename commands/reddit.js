72c4491d589f0bd559c86bcc7e2c2f0c69796c5b
Arniox <nikkdiehl@gmail.com>
2020-10-11 18:03:14 +1300

    wrongly named api call

--- commands/reddit.js
+++ commands/reddit.js
@@ -26,7 +26,7 @@ exports.run = (bot, guild, message, args) => {
                         }).then((res) => {
                             //Found sub reddit
                             //Get all flairs for this sub reddit
-                            bot.reddit.get(`${res.names[0]}/api/flairlist`, {
+                            bot.reddit.get(`/r/${res.names[0]}/api/flairlist`, {
                                 show: 'all'
                             }).then((res) => {
                                 //Found all flairs
