41a4b7d95c6b044b0a6f48ba7f5572ed191bf15d
Arniox <nikkdiehl@gmail.com>
2020-10-12 15:10:30 +1300

    fixed one issue

--- commands/redditpost.js
+++ commands/redditpost.js
@@ -6,7 +6,7 @@ exports.run = (bot) => {
     //Get all subscribed subreddits
     const sql_cmd = `SELECT * FROM server_subbed_reddits`;
     bot.con.query(sql_cmd, (error, results, fields) => {
-        if (error) throw error; //Return error console log and continu8e
+        if (error) return console.error(error); //Return error console log and continu8e
 
         //For each server
         bot.guilds.cache.map((value, key) => {
