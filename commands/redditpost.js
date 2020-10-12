6cab600b6f22fd8a81f7b25f88c6ffa87c6b6d5e
Arniox <nikkdiehl@gmail.com>
2020-10-12 15:11:36 +1300

    logging

--- commands/redditpost.js
+++ commands/redditpost.js
@@ -8,10 +8,14 @@ exports.run = (bot) => {
     bot.con.query(sql_cmd, (error, results, fields) => {
         if (error) return console.error(error); //Return error console log and continu8e
 
+        console.log(results);
+
         //For each server
         bot.guilds.cache.map((value, key) => {
             //Grab all the subscribed subreddits for this server
             var thisServer = results.filter(i => i.ServerId);
+
+            console.log(thisServer);
 
             //For each subscription
             thisServer.forEach((sub) => {
@@ -22,7 +26,6 @@ exports.run = (bot) => {
                 }).then((res) => {
                     //Found posts from this sub reddit
                     console.log(res);
-                    console.log(sub);
                 }).catch((err) => {
                     return console.error(err); //Throw error
                 });
