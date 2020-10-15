8b4b7ea85242c43e6612ca0736104e7445c80dce
Arniox <nikkdiehl@gmail.com>
2020-10-15 17:34:21 +1300

    Added emojis, tagging members and tagging channels as default patterns to remove from the message.
    - Changed the command deletion speed to 300 milliseconds instead of 100 as it was causing issues.
    - Added an Auto_Delete_Translation field to servers to auto delete the original message
    - Added an Embedded_Translation field to servers to send translations as embedded or flat normal messages.

--- commands/translatemessage.js
+++ commands/translatemessage.js
@@ -27,7 +27,11 @@ exports.run = (bot, guild, message, args) => {
                 if (error) return console.error(error); //Return error console log
 
                 //Cut out translationn ignored patterns
-                var messy = message.content;
+                //Cut out default ignored patterns
+                var messy = message.content
+                    .replace(/'<:[a-zA-Z]+:\d+>'/g, '') //Get rid of emojis
+                    .replace(/'<[@]?[!]?d+>'/g, '') //Get rid of tagging members
+                    .replace(/'<[#]?[!]?d+>'/g, ''); //Get rid of tagging channels
                 results.map(v => v.Pattern).forEach((e) => {
                     //Replace the text
                     messy = messy.replace(new RegExp(e, 'g'), '');
@@ -42,27 +46,34 @@ exports.run = (bot, guild, message, args) => {
                             //Translate
                             googleTranslate.translate(message.content, detection.language, 'en', function (err, translation) {
                                 if (translation.translatedText !== message.content) {
-                                    message.delete({ timeout: 0 }); //Delete message
+                                    //Auto delete messages if turned on
+                                    if (guild.Auto_Delete_Translation == 1) message.delete({ timeout: 300 }); //Delete message
                                     //Get country
                                     googleTranslate.getSupportedLanguages('en', function (err, languageCodes) {
-                                        //Create embedded message
-                                        var embeddedTranslation = new Discord.MessageEmbed()
-                                            .setColor('#0099ff')
-                                            .setAuthor(message.author.username, message.author.avatarURL())
-                                            .setDescription(translation.translatedText)
-                                            .addFields(
-                                                { name: 'Original text', value: `${message.content}` },
-                                                {
-                                                    name: 'Detected Language',
-                                                    value: `${languageCodes.find(i => i.language == detection.language).name} - ` +
-                                                        `${(detection.confidence * 100).floor().toString()}% confidence.`,
-                                                    inline: true
-                                                }
-                                            )
-                                            .setTimestamp()
-                                            .setFooter('Powered by Google Translate');
-                                        //Send
-                                        message.channel.send(embeddedTranslation);
+                                        //Check if server has embedded translation on
+                                        if (guild.Embedded_Translations == 1) {
+                                            //Create embedded message
+                                            var embeddedTranslation = new Discord.MessageEmbed()
+                                                .setColor('#0099ff')
+                                                .setAuthor(message.author.username, message.author.avatarURL())
+                                                .setDescription(translation.translatedText)
+                                                .addFields(
+                                                    { name: 'Original text', value: `${message.content}` },
+                                                    {
+                                                        name: 'Detected Language',
+                                                        value: `${languageCodes.find(i => i.language == detection.language).name} - ` +
+                                                            `${(detection.confidence * 100).floor().toString()}% confidence.`,
+                                                        inline: true
+                                                    }
+                                                )
+                                                .setTimestamp()
+                                                .setFooter('Powered by Google Translate');
+                                            //Send
+                                            message.channel.send(embeddedTranslation);
+                                        } else {
+                                            //Send normal message
+                                            message.channel.send(`${translation.translatedText} | **${languageCodes.find(i => i.language == detection.language).name}** | ${message.author.username}`);
+                                        }
                                     });
                                 }
                             });
