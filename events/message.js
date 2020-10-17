2730ddfcca683ff1d72b764cf634c81ae85c011f
Arniox <nikkdiehl@gmail.com>
2020-10-17 15:56:29 +1300

    added logging

--- events/message.js
+++ events/message.js
@@ -90,6 +90,8 @@ module.exports = (bot, message) => {
                             message.delete({ timeout: 200 }); //Delete message
                         }).catch(() => { return; });
                     } else if (evaluate(message.content)) {
+                        console.log('I can calculate this');
+
                         message.channel.send(new Discord.MessageEmbed().setDescription(`> ${NickName(message.member)} ${message.content}\n = ${evaluate(message.content).trimString(2048)}`).setColor('#0099ff'));
                     } else {
                         //Get the specific translatemessage command data from client.commands Enmap
