1ed855292d66d8d0dba66e06fbefc75b4678c12a
Arniox <nikkdiehl@gmail.com>
2021-01-05 03:07:51 +1300

    Forgot to split the array.

--- commands/music.js
+++ commands/music.js
@@ -247,7 +247,7 @@ exports.run = (bot, guild, message, command, args) => {
                     var output = '';
                     for (var i = 0; i < serverQueue.songs.length; i++) {
                         //Create output per song
-                        output += `${i} - ${serverQueue.songs[i].song.title} [${serverQueue.songs[i].queuedBy.toString()}]`;
+                        output += `${i} - ${serverQueue.songs[i].song.title} [${serverQueue.songs[i].queuedBy.toString()}]\n`;
                     }
                     return output;
                 }), 10, '#0099ff');
