436d7dbd28711abc87d14d7a76c8f9823c8564be
Arniox <nikkdiehl@gmail.com>
2021-01-07 18:18:07 +1300

    Added voiceStateUpdate event listener to check if bot is undeafened while playing music

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,21 @@
+//Import requirements
+const Discord = require('discord.js');
+
+module.exports = (bot, oldVoiceState, newVoiceState) => {
+    //Check if the updated user is me
+    if (oldVoiceState.guild.me == oldVoiceState.member) {
+        //Check if the bot has any active serverQueue for this guild
+        const serverQueue = bot.musicQueue.get(oldVoiceState.guild.id);
+        if (serverQueue) {
+            //Check if the bot was undeafened
+            if (!newVoiceState.deaf) {
+                //Send message
+                const textChannel = bot.guilds.cache.get(newVoiceState.guild.id).channels.cache.get(serverQueue.textChannel);
+                textChannel.send(new Discord.MessageEmbed().setDescription('Please do not undeafen me while I am playing music').setColor('#b50909'));
+
+                //Set bot to deaf
+                newVoiceState.guild.me.voice.setDeaf(true);
+            }
+        }
+    }
+}
